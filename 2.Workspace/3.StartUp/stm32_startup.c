b868e07a1a27adb40dff526ab36a634fd2216b78
ThanhPhammmm <phamvanthanh2522004@gmail.com>
2026-05-05 01:44:50 +0700

    update code

--- 2.Workspace/3.StartUp/stm32_startup.c
+++ 2.Workspace/3.StartUp/stm32_startup.c
@@ -6,6 +6,13 @@
 
 #define STACK_START     SRAM_END
 
+extern uint32_t _etext;
+extern uint32_t _sdata;
+extern uint32_t _edata;
+extern uint32_t _sbss;
+extern uint32_t _ebss;
+
+void main(void);
 void Reset_Handler(void);
 
 void NMI_Handler 					(void) __attribute__ ((weak, alias("Default_Handler")));
@@ -203,8 +210,21 @@ uint32_t vectorTable[] = {
 void Default_Handler(void){}
 void Reset_Handler(void){
     // Copy .data section of Flash to .data section of SRAM
+    uint32_t size = &_edata - &_sdata;
+
+    uint8_t *pDst = (uint8_t*)&_sdata;
+    uint8_t *pSrc = (uint8_t*)&_etext;
+
+    for(uint32_t i = 0;i < size;i++){
+        *pDst++ = *pSrc++;
+    }
 
     // Init the .bss section to zero in SRAM
-
+    size = &_ebss - &_sbss;
+    pDst = (uint8_t*)&_sbss;
+    for(uint32_t i = 0;i < size;i++){
+        *pDst++ = 0;
+    }
     // Call main()
+    main();
 }
